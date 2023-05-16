6952b12f7c9b0bdc693a9c470b13d844a8b7536a
Szpachlarz <kl.kolakowska@gmail.com>
2023-05-16 13:34:13 +0200

    add project files

---
+++ io-book-project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace io_book_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
