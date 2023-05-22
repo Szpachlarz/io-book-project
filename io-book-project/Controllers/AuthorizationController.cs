8fe53de21d090f5e876144e1e3bd999c5df0f5ca
Arek2017 <agront@interia.pl>
2023-05-22 19:08:46 +0200

    Sidebar and created views for login and register

---
+++ io-book-project/Controllers/AuthorizationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace io_book_project.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
