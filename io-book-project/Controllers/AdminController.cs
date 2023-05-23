e3f56f5c06232286d1800d1eb93eae2a9a3bb901
Arek2017 <agront@interia.pl>
2023-05-23 20:40:14 +0200

    Created admin layout and some views

---
+++ io-book-project/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace io_book_project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddAuthor()
+        {
+            return View();
+        }
+
+        public IActionResult AuthorList()
+        {
+            return View();
+        }
+
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+
+        public IActionResult BookList()
+        {
+            return View();
+        }
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryList()
+        {
+            return View();
+        }
+
+        public IActionResult AddPublishingHouse()
+        {
+            return View();
+        }
+
+        public IActionResult PublishingHouseList()
+        {
+            return View();
+        }
+    }
+}
