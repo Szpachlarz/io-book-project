609299c4d46388f9097ac6e416eb4418c3bdd9cc
Arek2017 <agront@interia.pl>
2023-05-28 22:32:14 +0200

    Created a view for user

---
+++ io-book-project/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace io_book_project.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BooksList()
+        {
+            return View();
+        }
+        public IActionResult ReviewsList()
+        {
+            return View();
+        }
+    }
+}
