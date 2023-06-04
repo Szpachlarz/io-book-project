e4f2c3becf75c2805dcedec4026c6d252d60f338
Arek2017 <agront@interia.pl>
2023-06-04 13:59:50 +0200

    Adding formData from AddBook to database (not finished yet)

--- io-book-project/Controllers/AdminController.cs
+++ io-book-project/Controllers/AdminController.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using io_book_project.Utils;
+using io_book_project.ViewModels;
+using io_book_project.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+using io_book_project.Data;
+
 
 namespace io_book_project.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly AppDbContext _context;
+        public AdminController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             if(HttpContext.Session.GetString(Const.USER_ROLE)!="admin")
@@ -25,9 +37,30 @@ namespace io_book_project.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult AddBook()
         {
+            //przekopiować z indexu to zabezpieczenie admina
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBook(AddBookViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var book = new Book
+                {
+                    Title = model.Title,
+                };
+
+                _context.Books.Add(book);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Admin");
+            }
+
+            return View("AddBook", model);
         }
 
         public IActionResult BookList()
---
+++ io-book-project/ViewModels/AddBookViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace io_book_project.ViewModels
+{
+    public class AddBookViewModel
+    {
+        [Display(Name = "Tytuł")]
+        [Required(ErrorMessage = "Nazwa książki jest wymagana")]
+        [StringLength(120, ErrorMessage = "Maksymalnie 120 znaków")]
+        public string Title { get; set; }
+    }
+}
