7407ce9a800ede987a397b7d47a608185c52670b
Arek2017 <agront@interia.pl>
2023-06-20 21:37:57 +0200

    added pagination in admin lists (not working in BookList)

--- io-book-project/ViewModels/BookListViewModel.cs
+++ io-book-project/ViewModels/BookListViewModel.cs
@@ -4,7 +4,8 @@ namespace io_book_project.ViewModels
 {
     public class BookListViewModel
     {
-        public IEnumerable<Book> Books;
+        public IEnumerable<Book> Books { get; set; }
         public List<List<Author>> Authors { get; set; }
+        public List<Book> Data { get; set; }
     }
 }
